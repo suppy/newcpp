e6e2015c697db7a464c7409aa707dfdb9fcf0514
suyama <suyama.ikuo@matsue-ct.jp>
2016-06-01 16:37:29 +0900

    add hito_sample2.cpp

---
+++ 22_hito_sample2.cpp
@@ -0,0 +1,51 @@
+//hito_sample2.cpp
+#include <iostream>
+using namespace std;
+
+//人を表すクラス
+class Hito
+{
+	int power;
+public:
+	Hito(int x) : power(x) {}
+	void set_power(int x) { power = x; }
+	int get_power() const { return power; }
+	void Jikosyoukai(); //自己紹介関数
+};
+
+//Hitoの派生クラスSamurai
+class Samurai : public Hito
+{
+public:
+	//Hitoのコンストラクタにxを渡す以外は何もしないコンストラクタ
+	Samurai(int x) : Hito(x) {}
+	//Hitoと同名のメンバ関数
+	void Jikosyoukai();
+};
+
+void Hito::Jikosyoukai()
+{
+	power--; //自己紹介に力を使ってpowerを1減らすことにする
+	cout << "俺は人だ。" << endl;
+	cout << "俺のパワーは" << power << "だ。" << endl;
+}
+
+void Samurai::Jikosyoukai()
+{
+	set_power(get_power() - 1);
+	//上はpowerの値をget_powerで取り出し、それから1減らした値をpowerにセットしている
+	//つまり、これでpowerを1減らしたことになる
+	cout << "俺はさむらいだ。" << endl;
+	cout << "俺のパワーは" << get_power() << "だ。" << endl;
+}
+
+int main()
+{
+	Hito *p1, *p2; //Hitoオブジェクトへのポインタ。まだ、何も代入されていない。
+	p1 = new Hito(10); //パワーが10のHitoオブジェクトが生成され、そのアドレスがp1に代入される。
+	p1->Jikosyoukai(); //p1の指し示すオブジェクトについてJkosyoukai()を呼ぶ。
+	p2 = new Samurai(12); //パワーが12のSamuraiオブジェクトが生成され、そのアドレスがp2に代入される。
+	p2->Jikosyoukai(); //p2の指し示すオブジェクトについてJkosyoukai()を呼ぶ。
+	delete p1; //p1の指し示すオブジェクトを破棄。
+	delete p2; //p2の指し示すオブジェクトを破棄。
+}
