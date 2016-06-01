eec8426b4f01132d78bf46f5e414cebc64281680
suyama <suyama.ikuo@matsue-ct.jp>
2016-06-01 13:43:54 +0900

    add ex2.cpp

---
+++ 12_ex2.cpp
@@ -0,0 +1,15 @@
+//ex2.cpp
+#include <iostream>
+using namespace std;
+
+//関数func()を定義します。
+int func()
+{
+	cout << "こんにちは。私はコンピュータです。" << endl;
+	return 1;   //整数値１を戻す
+}
+
+int main()
+{
+	func();     //func()を使っている。
+}
