c05c87b35b5e04f727c95eedaa4f411a1d4f1bb2
suyama <suyama.ikuo@matsue-ct.jp>
2016-06-01 13:26:09 +0900

    add while_sample.cpp

---
+++ 11_while_sample.cpp
@@ -0,0 +1,13 @@
+//while_sample.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	i = 0;
+	while (i < 100) {
+		cout << i << "  "; //見やすさのため数字の後に空白をいれた
+		i++;                   //iを１増やせという意味です。（下を参照）
+	}
+}
