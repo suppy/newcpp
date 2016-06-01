70f9f6b3ac1b5a3ac854a94cee86fbe92630ecb9
suyama <suyama.ikuo@matsue-ct.jp>
2016-06-01 13:18:31 +0900

    add ransu2.cpp

---
+++ 10_ransu2.cpp
@@ -0,0 +1,14 @@
+//ransu2.cpp
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+}
